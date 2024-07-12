7809fb1c2c64c1b1eea2de7f8e2eb51ac8ab422f
Kalvin Liang Wu <kalvinliang123@gmail.com>
2024-07-11 21:00:40 -0400

    completed the basic css and html design for home page in basic css and html

---
+++ webpack.common.js
@@ -0,0 +1,39 @@
+const path = require("path");
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Restaurant",
+            template: "./src/index.html",
+            inject: "body",
+        }),
+    ],
+    optimization: {
+        runtimeChunk: "single",
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource'
+            }, 
+        ],
+    },
+}
