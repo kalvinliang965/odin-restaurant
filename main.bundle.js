94044708eb5f7b02aca7174bc3dc7fc4ac5d3b9e
Kalvin Liang Wu <kalvinliang123@gmail.com>
2024-07-12 16:59:01 -0400

    remove dist from .gitignore

---
+++ main.bundle.js
@@ -0,0 +1,2 @@
+(self.webpackChunkodin_restaurant=self.webpackChunkodin_restaurant||[]).push([[792],{44:()=>{console.log("Hello, world!")}},e=>{e(e.s=44)}]);
+//# sourceMappingURL=main.bundle.js.map
